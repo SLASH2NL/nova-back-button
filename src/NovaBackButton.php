c651933778e35f840da4d468da1336084dd5d478
Jeffrey Zant <j.zant@slash2.nl>
2021-05-31 11:28:31 +0200

    adding default nova styles for the arrow svg

--- src/NovaBackButton.php
+++ src/NovaBackButton.php
@@ -29,7 +29,7 @@ class NovaBackButton extends Card
             'withoutCardStyles' => true,
             'forceFullWidth' => false,
             'content' => '<span class="btn btn-icon btn-default btn-white" style="position: absolute; top: -2.25rem;">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
             </span>',
