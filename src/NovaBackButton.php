5e8d54ae1578a42a61b317e3e50637f90979d2e1
Jeffrey Zant <j.zant@slash2.nl>
2022-04-11 16:33:25 +0200

    use arrow icon as default instead of "back"

--- src/NovaBackButton.php
+++ src/NovaBackButton.php
@@ -47,7 +47,9 @@ class NovaBackButton extends Card
                     h-9
                     text-base
                 ">
-                    Back
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                    </svg>
                 </span>
             ',
             'url' => null,
